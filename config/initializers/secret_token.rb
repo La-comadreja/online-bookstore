6015f7e938bdf8e10ff6f257d46a7a0b023f884d
Rebecca Anne Sealfon <ras2198@columbia.edu>
2015-10-31 15:05:30 -0400

    Initialized empty Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OnlineBookstore::Application.config.secret_key_base = '1f3928cd983c6c7af0e27aba6038535eaaf16b8cb8f3fb5617c28962b81116d6f3149144b5625b51866a5cff2ba0e4ea7447b38fcdd8c6049f725cdd0aa295d0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OnlineBookstore::Application.config.session_store :cookie_store, key: '_online-bookstore_session'
